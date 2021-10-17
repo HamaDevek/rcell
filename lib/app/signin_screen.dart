64ffa64492f88cadf5918a0038151ea51a2d3e98
wizard-technology <63434176+wizard-technology@users.noreply.github.com>
2021-10-17 10:16:45 +0300

    Update signin_screen.dart

--- lib/app/signin_screen.dart
+++ lib/app/signin_screen.dart
@@ -32,83 +32,89 @@ class _SignInScreenState extends State<SignInScreen> {
         child: Center(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 17),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: 121,
-                    ),
-                    Row(children: [
-                      IconComponent(
-                        size: 48,
-                        fontSize: 21,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 121,
                       ),
-                    ]),
-                    SizedBox(
-                      height: 45,
-                    ),
-                    SizedBox(
-                      width: double.infinity,
-                      child: Text(
-                        labels['btn.signin'] ?? '',
-                        style: Theme.of(context).textTheme.headline1,
+                      Row(children: [
+                        IconComponent(
+                          size: 48,
+                          fontSize: 21,
+                        ),
+                      ]),
+                      SizedBox(
+                        height: 45,
                       ),
-                    ),
-                    SizedBox(
-                      height: 11,
-                    ),
-                    Text(
-                      labels['message.signin'] ?? '',
-                      style: Theme.of(context).textTheme.bodyText1,
-                    ),
-                    SizedBox(
-                      height: 45,
-                    ),
-                    TextfieldPrimaryComponent(
-                      textHint: labels['label.email'] ?? '',
-                      controller: _emailController,
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    TextfieldPrimaryComponent(
-                      textHint: labels['label.password'] ?? '',
-                      obscureText: true,
-                      controller: _passwordController,
-                    ),
-                    SizedBox(
-                      height: 35,
-                    ),
-                    ButtonPrimaryComponent(
-                      child: Text(labels['btn.signin'] ?? ''),
-                      fontWeight: FontWeight.w700,
-                      onPress: () {
-                        Navigator.of(context).pushReplacementNamed('/success');
-                      },
-                    ),
-                    Container(
-                      height: 1,
-                      width: double.infinity,
-                      color: Theme.of(context).colorScheme.secondary,
-                      margin: EdgeInsets.symmetric(
-                        vertical: 35,
+                      SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                          labels['btn.signin'] ?? '',
+                          style: Theme.of(context).textTheme.headline1,
+                        ),
                       ),
-                    ),
-                    ButtonSecondaryComponent(
-                      child: Text(labels['btn.auto.signin'] ?? ''),
-                      fontWeight: FontWeight.w700,
-                      onPress: () {
-                        _emailController!.text = 'meer@netspot.com';
-                        _passwordController!.text = 'meer@netspot.com';
-                      },
-                    ),
-                  ],
-                ),
-                FooterComponent(),
-              ],
+                      SizedBox(
+                        height: 11,
+                      ),
+                      Text(
+                        labels['message.signin'] ?? '',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                      SizedBox(
+                        height: 45,
+                      ),
+                      TextfieldPrimaryComponent(
+                        textHint: labels['label.email'] ?? '',
+                        controller: _emailController,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextfieldPrimaryComponent(
+                        textHint: labels['label.password'] ?? '',
+                        obscureText: true,
+                        controller: _passwordController,
+                      ),
+                      SizedBox(
+                        height: 35,
+                      ),
+                      ButtonPrimaryComponent(
+                        child: Text(labels['btn.signin'] ?? ''),
+                        fontWeight: FontWeight.w700,
+                        onPress: () {
+                          Navigator.of(context)
+                              .pushReplacementNamed('/success');
+                        },
+                      ),
+                      Container(
+                        height: 1,
+                        width: double.infinity,
+                        color: Theme.of(context).colorScheme.secondary,
+                        margin: EdgeInsets.symmetric(
+                          vertical: 35,
+                        ),
+                      ),
+                      ButtonSecondaryComponent(
+                        child: Text(labels['btn.auto.signin'] ?? ''),
+                        fontWeight: FontWeight.w700,
+                        onPress: () {
+                          _emailController!.text = 'meer@netspot.com';
+                          _passwordController!.text = 'meer@netspot.com';
+                        },
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 45,
+                  ),
+                  FooterComponent(),
+                ],
+              ),
             ),
           ),
         ),
